3139ef92b84b803b3dc476c8626b0bbb69a918e2
PhiloestThio <thiophiloes@gmail.com>
2017-09-29 20:09:44 +0700

    pertemuan 2
    29 september 2017

---
+++ pert2/dosen.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<h1>Data Dosen</h1>
+<table>
+	<thead>
+		<td>No.</td>
+		<td>Kode Dosen</td>
+		<td>Nama</td>
+	</thead>
+	<tbody>
+		<tr>
+			<td>1</td>
+			<td>L007</td>
+			<td>John Doe</td>
+		</tr>
+	</tbody>
+</table>
